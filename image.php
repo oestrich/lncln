237bf1f62d45439c02920f530167498c2c0e5551
Eric Oestrich <eric@oestrich.org>
2009-02-16 17:01:14 +0000

    Moved image.php over to iconActions.php
    git-svn-id: file:///home/oestrich/tmp/img/trunk@99 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- image.php
+++ image.php
@@ -16,67 +16,7 @@ connect();
 $lncln = new lncln();
 $lncln->loggedIn();
 
-
-if(isset($_GET['obscene']) && $isLoggedIn){
-	$obscene = obscene($_GET['obscene']);
-	$image = $_GET['obscene'];
-}
-
-if(isset($_GET['rateUp']) && $isLoggedIn){
-	$rating = 1;
-	if($isAdmin){
-		$rating = 5;
-	}
-	rate($_GET['rateUp'], $userID, $rating);
-	$image = $_GET['rateUp'];
-}
-
-if(isset($_GET['rateDown']) && $isLoggedIn){
-	$rating = -1;
-	if($isAdmin){
-		$rating = -5;
-	}
-	rate($_GET['rateDown'], $userID, $rating);
-	$image = $_GET['rateDown'];
-}
-
-if($_GET['viewObscene']){
-	if($_COOKIE['obscene'] == false || !$_COOKIE['obscene']){
-		setcookie('obscene', true, time() + (60 * 60 * 24));
-	}
-	else{
-		setcookie('obscene', true, time() - (60 * 60 * 24));
-	}
-	header("location:index.php");	
-	exit();
-}
-
-if(isset($_GET['refresh']) && $isLoggedIn){
-	$id = stripslashes($_GET['refresh']);
-	$id = mysql_real_escape_string($id);
-	
-	$sql = "SELECT type FROM images WHERE id = " . $id;
-	$result = mysql_query($sql);
-	
-	if(mysql_num_rows($result) == 1){
-		$row = mysql_fetch_assoc($result);
-		thumbnail($id . "." . $row['type'], $curURL);
-	}
-	header("location:image.php?img=" . $id);
-	exit();
-}
-
-if($_GET['caption'] && $isLoggedIn){
-	caption($_POST['id'], $_POST['caption']);
-	header("location:image.php?img=" . $_GET['img']);
-	exit();
-}
-
-if($_GET['tag'] && $isLoggedIn){
-	tag($_POST['id'], $_POST['tags']);
-	header("location:image.php?img=" . $_GET['img']);
-	exit();
-}
+require_once("includes/iconActions.php");
 
 require_once("header.php");
 
