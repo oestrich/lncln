ed40ddad20489d3faadf5367be199fa0455ed6c3
Eric Oestrich <eric@oestrich.org>
2009-02-17 22:18:09 +0000

    Updating the theme.
    git-svn-id: file:///home/oestrich/tmp/img/trunk@135 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- includes/footer.php
+++ includes/footer.php
@@ -9,12 +9,13 @@
  */
 ?>
 
-</div>
-<div id="footer">
-	Any image upload by an anonymous user is put into a moderation queue.  Moderation is done by the discretion of the admins.<br />
-	If you find something that belongs to you, and would like it taken down, please contact mazra[at]boomboxlincoln[dot]org<br />
-	This was designed for Firefox - Powered by lncln <?echo VERSION;?>
-</div>
+		</div>
+		<div id="footer">
+			Any image upload by an anonymous user is put into a moderation queue.  Moderation is done by the discretion of the admins.<br />
+			If you find something that belongs to you, and would like it taken down, please contact <a href="mailto:mazra[at]boomboxlincoln[dot]org">mazra</a><br />
+			Designed for Firefox - Powered by lncln <?echo VERSION;?>
+		</div>
+	</div>
 <?require_once("googleAnalytics.html");?> 
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -23,64 +23,11 @@
 	</script>
 </head>
 <body>
-<div>
-	<a href="javascript:;" onmousedown="toggleDiv('regular')">Upload</a> <a href="javascript:;" onmousedown="toggleDiv('url')">Upload from URL</a>
-</div>
-
-<!-- upload form -->
-<form enctype="multipart/form-data" action="index.php?post=true" method="post"  id="form" style="display: none;">
-	<div>
-		<input type="hidden" name="type" id="formType" value="regular" />
-		Uploaded files will be moderated. Tags are mandatory.<br />
-		Obscene<br />
-		<input name="upload0check" type="checkbox" value='true' class="obCheck"/>
-		<input name="upload0" id="upload0" type="file" onchange="TestFileType(this.form.upload0.value, ['', 'gif', 'jpg', 'png']);"/>
-		Tags: <input name="upload0tags" type="text" />
-		<br />
-		<input name="upload1check" type="checkbox" value='true' class="obCheck"/>
-		<input name="upload1" id="upload1" type="file" onchange="TestFileType(this.form.upload1.value, ['', 'gif', 'jpg', 'png']);"/>
-		Tags: <input name="upload1tags" type="text" />
-		<br />
-		<input name="upload2check" type="checkbox" value='true' class="obCheck"/>
-		<input name="upload2" id="upload2" type="file" onchange="TestFileType(this.form.upload2.value, ['', 'gif', 'jpg', 'png']);"/>
-		Tags: <input name="upload2tags" type="text" />
-		<br />
-		<input name="upload3check" type="checkbox" value='true' class="obCheck"/>
-		<input name="upload3" id="upload3" type="file" onchange="TestFileType(this.form.upload3.value, ['', 'gif', 'jpg', 'png']);"/>
-		Tags: <input name="upload3tags" type="text" />
-		<br />
-		<input name="upload4check" type="checkbox" value='true' class="obCheck"/>
-		<input name="upload4" id="upload4" type="file" onchange="TestFileType(this.form.upload4.value, ['', 'gif', 'jpg', 'png']);"/>
-		Tags: <input name="upload4tags" type="text" />
-		<br />
-		<input name="upload5check" type="checkbox" value='true' class="obCheck"/>
-		<input name="upload5" id="upload5" type="file" onchange="TestFileType(this.form.upload5.value, ['', 'gif', 'jpg', 'png']);"/>
-		Tags: <input name="upload5tags" type="text" />
-		<br />
-		<input name="upload6check" type="checkbox" value='true' class="obCheck"/>
-		<input name="upload6" id="upload6" type="file" onchange="TestFileType(this.form.upload6.value, ['', 'gif', 'jpg', 'png']);"/>
-		Tags: <input name="upload6tags" type="text" />
-		<br />
-		<input name="upload7check" type="checkbox" value='true' class="obCheck"/>
-		<input name="upload7" id="upload7" type="file" onchange="TestFileType(this.form.upload7.value, ['', 'gif', 'jpg', 'png']);"/>
-		Tags: <input name="upload7tags" type="text" />
-		<br />
-		<input name="upload8check" type="checkbox" value='true' class="obCheck"/>
-		<input name="upload8" id="upload8" type="file" onchange="TestFileType(this.form.upload8.value, ['', 'gif', 'jpg', 'png']);"/>
-		Tags: <input name="upload8tags" type="text" />
-		<br />
-		<input name="upload9check" type="checkbox" value='true' class="obCheck"/>
-		<input name="upload9" id="upload9" type="file" onchange="TestFileType(this.form.upload9.value, ['', 'gif', 'jpg', 'png']);"/>
-		Tags: <input name="upload9tags" type="text" />
-		<br />
-		<input type="submit" value="Upload File" />
-		<br />
-		Please view all images before uploading.
-	</div>
-</form>
-
-<div id="navBar">
-	<a href='<?echo URL;?>index.php'>Newest</a>
+	<div id="container">
+		<div id="header">
+			<img src="theme/images/abe.png" />
+			<div id="navBar">
+				<a href='<?echo URL;?>index.php'>Newest</a>
 <?
 	if($_GET['thumb']){
 		$thumb = "";
@@ -91,18 +38,18 @@
 		$onOff = "On";
 	}
 ?>
-	<a href='<?echo URL;?>index.php?img=<?echo $lncln->firstImage . $thumb;?>'>Thumbnail view (<?echo $onOff;?>)</a>
+				<a href='<?echo URL;?>index.php?img=<?echo $lncln->firstImage . $thumb;?>'>Thumbnail view (<?echo $onOff;?>)</a>
 <?
 if($lncln->isLoggedIn){
 ?>
-	<a href='<?echo URL;?>logout.php'>Log out <?echo $_COOKIE['username'];?></a>
-	<a href='<?echo URL;?>user.php'>Change Settings</a>
+				<a href='<?echo URL;?>logout.php'>Log out <?echo $_COOKIE['username'];?></a>
+				<a href='<?echo URL;?>user.php'>Change Settings</a>
 <?
 }
 else{
 ?>
-	<a href='<?echo URL;?>login.php'>Log in</a>
-	All images will be directed to the queue.
+				<a href='<?echo URL;?>login.php'>Log in</a>
+				All images will be directed to the queue.
 <?
 }
 
@@ -112,13 +59,69 @@ if($lncln->isAdmin){
 	$result = mysql_fetch_assoc($result);
 
 ?>
-	<a href='<?echo URL;?>admin/queue.php'>Check the Queue (<?echo $result['COUNT(*)'];?>)</a>
-	<a href='<?echo URL;?>admin/adduser.php'>Add a user</a>
+				<a href='<?echo URL;?>admin/queue.php'>Check the Queue (<?echo $result['COUNT(*)'];?>)</a>
+				<a href='<?echo URL;?>admin/adduser.php'>Add a user</a>
 <?
 }
 ?>
-	<br />
-	<a href='<?echo URL;?>index.php?viewObscene=true'>View Obscene</a>
+				<br />
+				<a href="javascript:;" onmousedown="toggleDiv('regular')">Upload</a> <a href="javascript:;" onmousedown="toggleDiv('url')">Upload from URL</a>
+				
+				
+				<!-- upload form -->
+				<form enctype="multipart/form-data" action="index.php?post=true" method="post"  id="form" style="display: none;">
+					<div>
+						<input type="hidden" name="type" id="formType" value="regular" />
+						Uploaded files will be moderated. Tags are mandatory.<br />
+						Obscene<br />
+						<input name="upload0check" type="checkbox" value='true' class="obCheck"/>
+						<input name="upload0" id="upload0" type="file" onchange="TestFileType(this.form.upload0.value, ['', 'gif', 'jpg', 'png']);"/>
+						Tags: <input name="upload0tags" type="text" />
+						<br />
+						<input name="upload1check" type="checkbox" value='true' class="obCheck"/>
+						<input name="upload1" id="upload1" type="file" onchange="TestFileType(this.form.upload1.value, ['', 'gif', 'jpg', 'png']);"/>
+						Tags: <input name="upload1tags" type="text" />
+						<br />
+						<input name="upload2check" type="checkbox" value='true' class="obCheck"/>
+						<input name="upload2" id="upload2" type="file" onchange="TestFileType(this.form.upload2.value, ['', 'gif', 'jpg', 'png']);"/>
+						Tags: <input name="upload2tags" type="text" />
+						<br />
+						<input name="upload3check" type="checkbox" value='true' class="obCheck"/>
+						<input name="upload3" id="upload3" type="file" onchange="TestFileType(this.form.upload3.value, ['', 'gif', 'jpg', 'png']);"/>
+						Tags: <input name="upload3tags" type="text" />
+						<br />
+						<input name="upload4check" type="checkbox" value='true' class="obCheck"/>
+						<input name="upload4" id="upload4" type="file" onchange="TestFileType(this.form.upload4.value, ['', 'gif', 'jpg', 'png']);"/>
+						Tags: <input name="upload4tags" type="text" />
+						<br />
+						<input name="upload5check" type="checkbox" value='true' class="obCheck"/>
+						<input name="upload5" id="upload5" type="file" onchange="TestFileType(this.form.upload5.value, ['', 'gif', 'jpg', 'png']);"/>
+						Tags: <input name="upload5tags" type="text" />
+						<br />
+						<input name="upload6check" type="checkbox" value='true' class="obCheck"/>
+						<input name="upload6" id="upload6" type="file" onchange="TestFileType(this.form.upload6.value, ['', 'gif', 'jpg', 'png']);"/>
+						Tags: <input name="upload6tags" type="text" />
+						<br />
+						<input name="upload7check" type="checkbox" value='true' class="obCheck"/>
+						<input name="upload7" id="upload7" type="file" onchange="TestFileType(this.form.upload7.value, ['', 'gif', 'jpg', 'png']);"/>
+						Tags: <input name="upload7tags" type="text" />
+						<br />
+						<input name="upload8check" type="checkbox" value='true' class="obCheck"/>
+						<input name="upload8" id="upload8" type="file" onchange="TestFileType(this.form.upload8.value, ['', 'gif', 'jpg', 'png']);"/>
+						Tags: <input name="upload8tags" type="text" />
+						<br />
+						<input name="upload9check" type="checkbox" value='true' class="obCheck"/>
+						<input name="upload9" id="upload9" type="file" onchange="TestFileType(this.form.upload9.value, ['', 'gif', 'jpg', 'png']);"/>
+						Tags: <input name="upload9tags" type="text" />
+						<br />
+						<input type="submit" value="Upload File" />
+						<br />
+						Please view all images before uploading.
+					</div>
+				</form>
+	
+	
+				<a href='<?echo URL;?>index.php?viewObscene=true'>View Obscene</a>
 <?
 	if($_COOKIE['obscene']){
 ?>
@@ -136,21 +139,22 @@ if($lncln->isAdmin){
 	
 	if(file_exists("bblincoln-latest.tar.gz")){
 ?>
-	<br />
-	<a href="<?echo URL;?>bblincoln-latest.tar.gz">Download them all</a> Updates at 1AM PST.
+				<br />
+				<a href="<?echo URL;?>bblincoln-latest.tar.gz">Download them all</a> Updates at 1AM PST.
 <?	
 	}
 ?>
-	<br />
-	We have <?echo $result['COUNT(*)'];?> images.
-	<br />
-	<form id='search' enctype="multipart/form-data" action="<?echo URL;?>search.php" method="post">
-		<div>
-			Tag search:
-			<input type='text' name='search' />
-			<input type='submit' value='Search' />
+				<br />
+				We have <?echo $result['COUNT(*)'];?> images.
+				<br />
+				<form id='search' enctype="multipart/form-data" action="<?echo URL;?>search.php" method="post">
+					<div>
+						Tag search:
+						<input type='text' name='search' />
+						<input type='submit' value='Search' />
+					</div>
+				</form>
+			</div>
 		</div>
-	</form>
-</div>
-<div id="mainBody">
-	<br />
+		<div id="mainBody">
+			<br />
