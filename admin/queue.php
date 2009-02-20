8b781e9f572dad135c1fd9dddb73eee35ff0bfb0
Eric Oestrich <eric@oestrich.org>
2009-02-20 00:19:35 +0000

    Should be good now.  Just added target="_blank" so it can open in a new window
    git-svn-id: file:///home/oestrich/tmp/img/trunk@170 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- admin/queue.php
+++ admin/queue.php
@@ -80,7 +80,7 @@ if($lncln->isAdmin){
 		
 			<input type='checkbox' name='<?echo $image['id'];?>' value='<?echo $image['id'];?>' style="float: left;" />
 			<div class="imageLink">
-				<a name="<?echo $image['id'];?>" href="<?=URL;?>images/full/<?echo $image['file'];?>"><img src="<?=URL;?>images/thumb/<?echo $image['file'];?>" alt="<?echo $image['id'];?>" /></a><br />
+				<a name="<?echo $image['id'];?>" href="<?=URL;?>images/full/<?echo $image['file'];?>" target="_blank"><img src="<?=URL;?>images/thumb/<?echo $image['file'];?>" alt="<?echo $image['id'];?>" /></a><br />
 				<a href="queue.php?obscene=<? echo $image['id'];?>" class="delete"><img src="<?=URL;?>theme/<?=THEME;?>/images/obscene.png" alt="Obscene" title="Obscene" style='border: none;'/></a>
 				<a href="queue.php?delete=<? echo $image['id'];?>"><img src="<?=URL;?>theme/<?=THEME;?>/images/delete.png" alt="Delete" title="Delete" style='border: none;'/></a><br />
 				<div class='delete'>Tags: <?echo $tags;?></div>
