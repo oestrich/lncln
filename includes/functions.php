fd5084b5d8d129b217203ef3ed269fb78b5e0c64
Eric Oestrich <eric@oestrich.org>
2009-02-27 16:29:25 +0000

    This should show albums on the image page now.
    git-svn-id: file:///home/oestrich/tmp/img/trunk@223 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- includes/functions.php
+++ includes/functions.php
@@ -135,10 +135,20 @@ class lncln{
 		if(mysql_num_rows($result) == 1){
 			$image = mysql_fetch_assoc($result);
 			
+			if($image['album'] != 0){
+				$sql = "SELECT name FROM albums WHERE id = " . $image['album'];
+				$album = mysql_query($sql);
+				$album = mysql_fetch_assoc($album);
+			}
+			else{
+				$album['name'] = "No Album";
+			}
+			
 			$this->images[0] = array(
 				'id' 		=> $image['id'],
 				'file' 		=> $image['id'] . "." . $image['type'],
 				'type'		=> $image['type'],
+				'album'		=> $album['name'],
 				'obscene' 	=> $image['obscene'],
 				'rating' 	=> $image['rating'],
 				'postTime'	=> $image['postTime'],
