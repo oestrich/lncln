6398743d162b4583de785342793bcbf4d112f744
Eric Oestrich <eric@oestrich.org>
2009-03-02 20:00:16 +0000

    bug #2, variables were plural, whoops
    git-svn-id: file:///home/oestrich/tmp/img/trunk@280 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- includes/functions.php
+++ includes/functions.php
@@ -492,7 +492,7 @@ class lncln{
 		if ($this->firstImage == $this->highestID && $this->lastImage != $this->lowestID){
 	        return "<a href='" . $this->script . "?img=" . $this->belowFifty . $extra . "' class='prevNext'>Next 50</a>";
 	    }
-	    elseif($this->firstImages == $this->highestId && $this->lastImages == $this->lowestID){
+	    elseif($this->firstImage == $this->highestId && $this->lastImage == $this->lowestID){
 	    	return "";
 	    }
 	    elseif($this->lastImage == $this->lowestID){
