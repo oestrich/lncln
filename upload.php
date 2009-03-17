e45a8ba41d0d2ffca4804bef7fe010e93ac93d40
Eric Oestrich <eric@oestrich.org>
2009-03-17 00:18:53 +0000

    Adding upload.php which will be the new upload drop off, where you can edit tags and what not before it is added to the homepage.  Also the drop off for mass uploading
    git-svn-id: file:///home/oestrich/tmp/img/trunk@364 ea04b438-0a61-0410-93d0-9bc465a3bab0

---
+++ upload.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * upload.php
+ * 
+ * @copyright (C) 2009 Eric Oestrich
+ * @version 0.9.0  $Id$
+ * @license license.txt GNU General Public License version 3
+ * 
+ * @package lncln
+ */
+ 
+ 
+?>
