dda5ec4fc4a0d71e5f4e8e8fff32a2a3b27fbde3
Eric Oestrich <eric@oestrich.org>
2009-03-24 06:16:28 +0000

    Checking for tags, if no tag, skips it and says too bad.
    git-svn-id: file:///home/oestrich/tmp/img/trunk@381 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- upload.php
+++ upload.php
@@ -14,6 +14,11 @@ require_once("load.php");
 if($_GET['action'] == "finishUpload"){
 	$lncln = new lncln();
 	foreach($_POST['check'] as $key => $value){
+	    if($_POST['images'][$key]['tags'] == ""){
+            $_SESSION['upload'][$i] = 3;
+            $lncln->delete($key);
+            continue;
+        }		
 		$lncln->tag($key, $_POST['images'][$key]['tags']);
 		$lncln->caption($key, $_POST['images'][$key]['caption']);
 		$lncln->changeAlbum($key, $_POST['images'][$key]['album']);
