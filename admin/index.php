c27487aaf484909d9c1e3ef84dc5ae9e334290a2
Eric Oestrich <eric@oestrich.org>
2009-04-08 21:27:08 +0000

    Changing the administration panel.  Or should I say creating one.
    git-svn-id: file:///home/oestrich/tmp/img/trunk@479 ea04b438-0a61-0410-93d0-9bc465a3bab0

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?
+/**
+ * index.php
+ * 
+ * Main administration panel for lncln
+ * 
+ * @copyright (C) 2009 Eric Oestrich
+ * @version 0.10.0 $Id$
+ * @license license.txt GNU General Public License version 3
+ * 
+ * @package lncln
+ */
+
+?>
