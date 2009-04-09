421463edb85d3ef22a77fc6498d1ce2b5dc70ad9
Eric Oestrich <eric@oestrich.org>
2009-04-09 20:31:13 +0000

    Making my own h3
    git-svn-id: file:///home/oestrich/tmp/img/trunk@499 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- admin/index.php
+++ admin/index.php
@@ -34,7 +34,7 @@ echo "Welcome to the admin panel";
 include($enabledModules[0] . "/info.php");
 
 foreach($links as $key => $module){
-	echo "<h3>" . $key . "</h3>";
+	echo "<span style='font-weight: bold; text-decoration:underline; font-size:large'>" . $key . "</span>";
 	foreach($module  as $link){
 		echo "<a href='" . $enabledModules[0] . "/index.php?action=" . $link['url'] . "'>" . $link['name'] . "</a><br />";
 	}
