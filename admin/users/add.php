1c596af9a3b8bf65cff18f43bf0efe2e9f913272
Eric Oestrich <eric@oestrich.org>
2009-04-09 20:19:55 +0000

    Pulled the actions out of index.php
    git-svn-id: file:///home/oestrich/tmp/img/trunk@495 ea04b438-0a61-0410-93d0-9bc465a3bab0

---
+++ admin/users/add.php
@@ -0,0 +1,33 @@
+<?
+/**
+ * add.php
+ * 
+ * Let's an admin create a user, what else?
+ * 
+ * @copyright (C) 2009 Eric Oestrich
+ * @version 0.10.0 $Id$
+ * @license license.txt GNU General Public License version 3
+ * 
+ * @package lncln
+ */
+ 
+if(isset($_POST['username'])){
+	$added = $lncln->adduser($_POST);
+}
+
+if(isset($added)){
+	echo $added;
+}
+?>
+<form enctype="multipart/form-data" action="index.php?action=add" method="post">
+		<div id="adduser">
+		Username: <input type="text" name="username" /><br />
+		Password: <input type='password' name='password' /><br />
+		Password: <input type='password' name='passwordconfirm' /><br />
+		Admin: <select name="admin">
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+		<input type="submit" value="Add user" />
+	</div>
+</form>
--- admin/users/index.php
+++ admin/users/index.php
@@ -19,28 +19,11 @@ include("../admin.php");
 
 require_once(ABSPATH . "includes/header.php");
 
-if($_GET['action'] == 'add'){
-	if(isset($_POST['username'])){
-		$added = $lncln->adduser($_POST);
-	}
-	
-	if(isset($added)){
-		echo $added;
-	}
-	?>
-	<form enctype="multipart/form-data" action="index.php?action=add" method="post">
-			<div id="adduser">
-			Username: <input type="text" name="username" /><br />
-			Password: <input type='password' name='password' /><br />
-			Password: <input type='password' name='passwordconfirm' /><br />
-			Admin: <select name="admin">
-						<option value="0">No</option>
-						<option value="1">Yes</option>
-					</select>
-			<input type="submit" value="Add user" />
-		</div>
-	</form>
-	<?
+if(file_exists($_GET['action'] . ".php")){
+	include($_GET['action'] . ".php");
+}
+else{
+	echo "That action does not exist";
 }
 
 require_once(ABSPATH . "includes/footer.php");
