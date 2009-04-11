d9778060f2fe22671678c8f32eecef41c7fc07be
Eric Oestrich <eric@oestrich.org>
2009-04-11 02:08:38 +0000

    Added a skeleton module to base new modules upon
    git-svn-id: file:///home/oestrich/tmp/img/trunk@543 ea04b438-0a61-0410-93d0-9bc465a3bab0

---
+++ admin/skeleton/index.php
@@ -0,0 +1,32 @@
+<?
+/**
+ * index.php
+ * 
+ * Main page for the skeleton admin module
+ * 
+ * @copyright (C) 2009 Eric Oestrich
+ * @version 0.10.0 $Id$
+ * @license license.txt GNU General Public License version 3
+ * 
+ * @package lncln
+ */ 
+
+require_once("../../load.php");
+
+include("class.php");
+
+$lncln = new Skeleton();
+
+include("../admin.php");
+
+require_once(ABSPATH . "includes/header.php");
+
+if(file_exists($_GET['action'] . ".php")){
+	include($_GET['action'] . ".php");
+}
+else{
+	echo "That action does not exist";
+}
+
+require_once(ABSPATH . "includes/footer.php");
+?>
