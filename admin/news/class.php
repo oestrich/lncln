f3bccacea74f80538a88180f61f7f5501b6fdc21
Eric Oestrich <eric@oestrich.org>
2009-04-14 06:11:50 +0000

    Starting on the news module
    git-svn-id: file:///home/oestrich/tmp/img/trunk@573 ea04b438-0a61-0410-93d0-9bc465a3bab0

---
+++ admin/news/class.php
@@ -0,0 +1,16 @@
+<?
+/**
+ * class.php
+ * 
+ * Main class for the News module
+ * 
+ * @copyright (C) 2009 Eric Oestrich
+ * @version 0.10.0 $Id$
+ * @license license.txt GNU General Public License version 3
+ * 
+ * @package lncln
+ */ 
+ 
+class News extends lncln{
+
+}
---
+++ admin/news/info.php
@@ -0,0 +1,21 @@
+<?
+/**
+ * info.php
+ * 
+ * Contains information regarding the news admin module.
+ * 
+ * @copyright (C) 2009 Eric Oestrich
+ * @version 0.10.0 $Id$
+ * @license license.txt GNU General Public License version 3
+ * 
+ * @package lncln
+ */ 
+ 
+$name['news'] = "News";
+ 
+$links['news'][0]['name'] = "Add news";
+$links['news'][0]['url'] = "add";
+
+$links['news'][1]['name'] = "Manage news";
+$links['news'][1]['url'] = "manage";
+?>
