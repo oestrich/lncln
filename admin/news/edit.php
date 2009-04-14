dc2c34cc5dacb8d10e13777b58e8861957c444be
Eric Oestrich <eric@oestrich.org>
2009-04-14 06:42:52 +0000

    Starting on editing news items
    git-svn-id: file:///home/oestrich/tmp/img/trunk@587 ea04b438-0a61-0410-93d0-9bc465a3bab0

---
+++ admin/news/edit.php
@@ -0,0 +1,13 @@
+<?
+/**
+ * edit.php
+ * 
+ * Let's an admin edit a news item
+ * 
+ * @copyright (C) 2009 Eric Oestrich
+ * @version 0.10.0 $Id$
+ * @license license.txt GNU General Public License version 3
+ * 
+ * @package lncln
+ */
+?>
