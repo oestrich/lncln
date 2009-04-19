62f7ff470bef94f1983654db8d842e4ebb09b205
Eric Oestrich <eric@oestrich.org>
2009-04-19 20:21:23 +0000

    only change the images that were clicked on
    git-svn-id: file:///home/oestrich/tmp/img/trunk@674 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- admin/queue.php
+++ admin/queue.php
@@ -55,10 +55,10 @@ echo $lncln->prevNext();
 		<a href="<?=URL;?>images/full/<?=$image['file'];?>" target="_blank" class="modImage"><img src="<?=URL;?>images/thumb/<?=$image['file'];?>" /></a>
 		<div class="modForms">
 			<input type="hidden" name="images[<?=$image['id'];?>][id]" value="<?=$image['id'];?>" /><br />
-			Tags:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name='images[<?=$image['id'];?>][tags]' value="<?=$tags;?>" /><br />
-			Caption:&nbsp;<textarea name="images[<?=$image['id'];?>][caption]" rows="10" cols="50" wrap="off" ><?=$image['caption'];?></textarea><br />
+			Tags:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name='images[<?=$image['id'];?>][tags]' value="<?=$tags;?>" onfocus="modCheck('<?=$image['id'];?>')" /><br />
+			Caption:&nbsp;<textarea name="images[<?=$image['id'];?>][caption]" rows="10" cols="50" wrap="off" onfocus="modCheck('<?=$image['id'];?>')" ><?=$image['caption'];?></textarea><br />
 			Album:&nbsp;&nbsp;&nbsp;
-			<select name="images[<?=$image['id'];?>][album]">
+			<select name="images[<?=$image['id'];?>][album]" onfocus="modCheck('<?=$image['id'];?>')" >
 				<option value="0">No album</option>
 	<?foreach($lncln->getAlbums() as $album):?>
 		<?$selected = $album['name'] == $image['album'] ? "selected" : "";?>
