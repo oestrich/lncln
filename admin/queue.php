6ac8d75753392518d68af0c824e7dcd3f4f37679
Eric Oestrich <eric@oestrich.org>
2009-04-19 20:11:46 +0000

    Making the queue look like a queue, no functionality yet
    git-svn-id: file:///home/oestrich/tmp/img/trunk@669 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- admin/queue.php
+++ admin/queue.php
@@ -23,8 +23,6 @@ if($_GET['action'] == "update"){
 	exit();
 }
 
-include(ABSPATH . "includes/iconActions.php");
-
 $lncln->img();
 
 require_once("../includes/header.php");
@@ -41,14 +39,51 @@ echo "There are " . $result['COUNT(*)'] . " items in the queue.";
 ?>
 <br />
 This page will show the first 50 items in the queue<br />
-<form enctype="multipart/form-data" action="queue.php?action=update" method="post">
-	<div class="queue">
+
 <?
-	include(ABSPATH . "includes/listImages.php");
+echo $lncln->prevNext();
 ?>
-		<input type='submit' value='Submit' />
+<form action="queue.php?action=update<?=$img;?>" method="post">
+<?
+	foreach($lncln->images as $image):	
+		$tags = join(', ', $image['tags']);
+?>
+	<div id="<?=$image['id'];?>" class="modDiv">
+		<input type="checkbox" name="check[<?=$image['id'];?>]" id="check<?=$image['id'];?>" /><br />
+		Obscene: <select name="images[<?=$image['id'];?>][obscene]" onfocus="modCheck('<?=$image['id'];?>')">
+				<?if($image['obscene'] == 1):?>
+					<option value="1" selected>Yes</option><option value="0">No</option>
+				<?else:?>
+					<option value="1">Yes</option><option value="0" selected>No</option>
+				<?endif;?>
+				 </select>
+		<a href="<?=URL;?>images/full/<?=$image['file'];?>" target="_blank" class="modImage"><img src="<?=URL;?>images/thumb/<?=$image['file'];?>" /></a>
+		<div class="modForms">
+			<input type="hidden" name="images[<?=$image['id'];?>][id]" value="<?=$image['id'];?>" /><br />
+			Tags:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name='images[<?=$image['id'];?>][tags]' value="<?=$tags;?>" /><br />
+			Caption:&nbsp;<textarea name="images[<?=$image['id'];?>][caption]" rows="10" cols="50" wrap="off" ><?=$image['caption'];?></textarea><br />
+			Album:&nbsp;&nbsp;&nbsp;
+			<select name="images[<?=$image['id'];?>][album]">
+				<option value="0">No album</option>
+	<?foreach($lncln->getAlbums() as $album):?>
+		<?$selected = $album['name'] == $image['album'] ? "selected" : "";?>
+				<option value="<?=$album['id'];?>" <?=$selected;?>><?=$album['name'];?></option>
+	<?endforeach;?>
+			</select>
+		</div>
 	</div>
+<?
+	endforeach;
+?>
+	<input type="submit" value="Submit" />
 </form>
+<div id='bPrevNext'>
+<?
+	echo $lncln->prevNext();
+?>
+</div>	
+<?
+
 
 require_once("../includes/footer.php");
 ?>
