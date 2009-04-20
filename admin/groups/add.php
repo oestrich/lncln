a02a2df0f847acc933f2e57a0b7dd50f5f774173
Eric Oestrich <eric@oestrich.org>
2009-04-20 01:37:48 +0000

    The look of adding groups
    git-svn-id: file:///home/oestrich/tmp/img/trunk@689 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- admin/groups/add.php
+++ admin/groups/add.php
@@ -19,6 +19,47 @@
 				<td>Name:</td>
 				<td><input type="text" name="name" /></td>
 			</tr>
+			<tr>
+				<td>Directly to Index:</td>
+				<td><input type="text" name="index" /></td>
+			</tr>
+			<tr>
+				<td title="Number of images that go directly to the homepage">Number to Index:</td>
+				<td><input type="text" name="numIndex" /></td>
+			</tr>
+			<tr>
+				<td>Report Value:</td>
+				<td><input type="text" name="report" /></td>
+			</tr>
+			<tr>
+				<td>Rate Value:</td>
+				<td><input type="text" name="rate" /></td>
+			</tr>
+			<tr>
+				<td>Use obscene:</td>
+				<td><select name="obscene" ><option value="0">No</option><option value="1">Yes</option></select></td>
+			</tr>
+			<tr>
+				<td>Use refresh:</td>
+				<td><select name="refresh" ><option value="0">No</option><option value="1">Yes</option></select></td>
+			</tr>
+			<tr>
+				<td>Use delete:</td>
+				<td><select name="delete" ><option value="0">No</option><option value="1">Yes</option></select></td>
+			</tr>
+			<tr>
+				<td>Use caption:</td>
+				<td><select name="caption" ><option value="0">No</option><option value="1">Yes</option></select></td>
+			</tr>
+			<tr>
+				<td>Use tag:</td>
+				<td><select name="tag" ><option value="0">No</option><option value="1">Yes</option></select></td>
+			</tr>
+			<tr>
+				<td>Manage albums:</td>
+				<td><select name="album" ><option value="0">No</option><option value="1">Yes</option></select></td>
+			</tr>
 		</table>
+		<input type="submit" value="Submit" />
 	</div>
 </form>
