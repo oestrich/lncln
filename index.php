be9fdb9bfe6ba17b88c4332e6a48c5fca6559690
Eric Oestrich <eric@oestrich.org>
2009-04-27 05:25:23 +0000

    adding the date posted in the news title
    git-svn-id: file:///home/oestrich/tmp/img/trunk@777 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- index.php
+++ index.php
@@ -25,7 +25,10 @@ include_once(ABSPATH . "includes/header.php");
 $news = $lncln->getNews();
 ?>	
 	<div id="news"> 
-		<span style="font-weight: bold; text-decoration: underline" onclick="news()"><?echo $news['title'];?> <span style="color: #22FF00">(click to show)</span>:</span><br/>
+		<span style="font-weight: bold; text-decoration: underline" onclick="news()">
+			<?echo $news['title'];?> <span style="color: #22FF00">(click to show)</span>: on <?=date("m/d/Y", $news['postTime']);?>:
+		</span>
+		<br/>
 		<p id="actualNews">
 			<?echo $news['news'];?> 
 		</p>
