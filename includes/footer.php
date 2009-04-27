14046ab5d2f4b81bc3c127505f2b84cd4849ef5a
Eric Oestrich <eric@oestrich.org>
2009-04-27 21:15:36 +0000

    doesn't show the version number
    git-svn-id: file:///home/oestrich/tmp/img/trunk@796 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- includes/footer.php
+++ includes/footer.php
@@ -15,7 +15,7 @@
 		</div>
 		<div id="footer">
 			If you find something that belongs to you, and would like it taken down, please contact <a href="mailto:mazra[at]boomboxlincoln[dot]org">mazra</a><br />
-			Powered by <a href="http://lncln.com">lncln <?echo $display->settings['version'];?></a>
+			Powered by <a href="http://lncln.com">lncln <?echo $lncln->display->settings['version'];?></a>
 		</div>
 	</div>
 <?include_once(ABSPATH . "includes/googleAnalytics.html");?> 
