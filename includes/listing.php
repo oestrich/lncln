ea863f4b49f83e5e849a29386b0706a4d2dec05a
Eric Oestrich <eric@oestrich.org>
2009-05-01 05:10:58 +0000

    will now open a shrunken image
    git-svn-id: file:///home/oestrich/tmp/img/trunk@837 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- includes/listing.php
+++ includes/listing.php
@@ -59,7 +59,7 @@ foreach ($lncln->images as $image){
 	<?endif;?>
 
 	<?if(($image['obscene'] == 1 && (!$_COOKIE['obscene'] || !isset($_COOKIE['obscene']))) || $image['rating'] <= -10):?>
-			<div class="badImage" id="i<?echo $image['id'];?>">
+			<div class="badImage" id="b<?echo $image['id'];?>">
 	<?endif;?>
 	
 			<a name="<?echo $image['id'];?>" href="<?=URL;?>images/full/<?echo $image['file'];?>" target="_blank"><img src="<?=URL;?>images/<?echo $lncln->type;?>/<?echo $image['file'];?>" alt="<?echo $image['id'];?>" /></a>
