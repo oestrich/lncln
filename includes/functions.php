d7e4e6a274b0d5f464254c81317bb38ef2e31843
Eric Oestrich <eric@oestrich.org>
2009-05-03 00:43:17 +0000

    forgot to enable it
    git-svn-id: file:///home/oestrich/tmp/img/trunk@956 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- includes/functions.php
+++ includes/functions.php
@@ -66,7 +66,7 @@ class lncln{
 	
 	function loadModules(){
 		//Key is folder, value is class name
-		$this->modules = array("tags" => "Tags", "albums" => "Albums", "captions" => "Captions");
+		$this->modules = array("tags" => "Tags", "albums" => "Albums", "captions" => "Captions", "ratings" => "Ratings");
 		
 		$this->display->rows = array(1 => array("albums"),2 => array("tags"));
 		
