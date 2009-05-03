c442f1ef63af53faba7f0b60561c5d646399808f
Eric Oestrich <eric@oestrich.org>
2009-05-03 20:24:15 +0000

    cleaning up the outputed source
    git-svn-id: file:///home/oestrich/tmp/img/trunk@991 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- modules/ratings/class.php
+++ modules/ratings/class.php
@@ -155,9 +155,8 @@ class Ratings implements Module{
 	 */
 	public function icon($id, $action){
 		if($this->lncln->user->permissions['ratings'] == 1):
-			$output = "
-			<a href='$action&amp;action=ratings&amp;subAction=up&amp;id=$id'><img src='" .URL ."theme/" .THEME ."/images/up.png' alt='Up' title='Up' style='border: none;'/></a>
-			<a href='$action&amp;action=ratings&amp;subAction=down&amp;id=$id'><img src='" .URL ."theme/" .THEME ."/images/down.png' alt='Down' title='Down' style='border: none;'/></a>";
+			$output = "<a href='$action&amp;action=ratings&amp;subAction=up&amp;id=$id'><img src='" .URL ."theme/" .THEME ."/images/up.png' alt='Up' title='Up' style='border: none;'/></a>
+			<a href='$action&amp;action=ratings&amp;subAction=down&amp;id=$id'><img src='" .URL ."theme/" .THEME ."/images/down.png' alt='Down' title='Down' style='border: none;'/></a>\n";
 		endif;
 		
 		return $output;
