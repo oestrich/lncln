cd98a5beb4c19acf8974fdf7e74da3e8ecbc1719
Eric Oestrich <eric@oestrich.org>
2009-05-11 18:48:07 +0000

    moving where rss is located
    git-svn-id: file:///home/oestrich/tmp/img/trunk@1006 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- rss.php
+++ rss.php
@@ -1,24 +1,28 @@
-<? 
+<?
 /**
- * safe.rss
+ * rss.php
  * 
- * Only displays the non-obscene images
+ * Shows the rss
  * 
  * @copyright (C) 2009 Eric Oestrich
- * @version 0.12.0 $Id$
- * @license license.txt GNU General Public License
+ * @version 0.13.0 $Id$
+ * @license license.txt GNU General Public License version 3
  * 
  * @package lncln
  */
- 
+
 header('Content-type: text/xml'); 
 
-require_once("../load.php");
+require_once("load.php");
 
-$lncln->rss(array("safe"));
+$action = $_GET['action'];
+if($action == "")
+	$action = "safe";
+
+$lncln->rss(array($action));
 $lncln->img();
 
-$server = "http://" . $_SERVER['SERVER_NAME'] . str_replace("rss/safe.rss", "", $_SERVER['SCRIPT_NAME']);
+$server = "http://" . $_SERVER['SERVER_NAME'] . str_replace("rss/all.rss", "", $_SERVER['SCRIPT_NAME']);
 
 ?>
 <rss version="2.0">
@@ -26,7 +30,7 @@ $server = "http://" . $_SERVER['SERVER_NAME'] . str_replace("rss/safe.rss", "", 
 
 <title>The Archive</title>
 <link><?echo $server;?></link>
-<description>All Images - Minus Obscene</description>
+<description>All Images</description>
 <lastBuildDate>Mon, 26 Jan 2009 18:37:00 EST</lastBuildDate>
 <language>en-us</language>
 
@@ -38,7 +42,7 @@ foreach($lncln->images as $image){
 		<link><?echo $server;?>image.php?img=<?echo $image['id'];?></link>
 		<guid><?echo $server;?>image.php?img=<?echo $image['id'];?></guid>
 		<pubDate><?echo date('r', $image['postTime']);?></pubDate>
-		<description>		
+		<description>
 			<![CDATA[ <img src="<?=$lncln->getImagePath($image['id'], "index");?>" alt="<?echo $image['id'];?>"/> <br />
 			<a href="<?=$lncln->getImagePath($image['id'], "full");?>" />Larger</a><br />
 			<?foreach($lncln->modules as $module):
@@ -48,7 +52,6 @@ foreach($lncln->images as $image){
 			endforeach;?>]]>
 		</description>
 	</item>
-
 <?
 }
 ?>
