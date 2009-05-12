a864045f2a6d3875076f5cfe8d7997cf18452c56
Eric Oestrich <eric@oestrich.org>
2009-05-12 05:19:07 +0000

    that was stupid of me
    git-svn-id: file:///home/oestrich/tmp/img/trunk@1081 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- includes/functions.php
+++ includes/functions.php
@@ -73,16 +73,16 @@ class lncln{
 			elseif($_GET['module'] == "thumbnail"){
 				$this->module = "thumbnail";
 			}
-			elseif($_GET['module'] == "" || !isset($_GET['module'])){
-				header("location:" . URL . "index/");
-				exit();
-			}
 			else{			
 				$this->module = $_GET['module'];
 				$_SESSION['URL'] = $this->module . "/" . join($this->params, "/");
 			}
 			
 			$_SESSION['URL'] = str_replace(" ", "+", $_SESSION['URL']);
+		}
+		else{
+			header("location:" . URL . "index/");
+			exit();
 		}
 	}
 	
