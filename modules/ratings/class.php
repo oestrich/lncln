d05e650982e74c8df3dd59e35034b9d393b96e96
Eric Oestrich <eric@oestrich.org>
2009-05-12 02:08:26 +0000

    use new URL structure for icons
    git-svn-id: file:///home/oestrich/tmp/img/trunk@1033 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- modules/ratings/class.php
+++ modules/ratings/class.php
@@ -155,8 +155,8 @@ class Ratings implements Module{
 	 */
 	public function icon($id, $action){
 		if($this->lncln->user->permissions['ratings'] == 1):
-			$output = "\t\t\t<a href='$action&amp;action=ratings&amp;subAction=up&amp;id=$id'><img src='" .URL ."theme/" .THEME ."/images/up.png' alt='Up' title='Up' style='border: none;'/></a>
-			<a href='$action&amp;action=ratings&amp;subAction=down&amp;id=$id'><img src='" .URL ."theme/" .THEME ."/images/down.png' alt='Down' title='Down' style='border: none;'/></a>\n";
+			$output = "\t\t\t<a href='" .URL . "action/ratings/up/$id'><img src='" .URL . "theme/" .THEME ."/images/up.png' alt='Up' title='Up' style='border: none;'/></a>
+			<a href='" .URL . "action/ratings/down/id=$id'><img src='" .URL . "theme/" .THEME ."/images/down.png' alt='Down' title='Down' style='border: none;'/></a>\n";
 		endif;
 		
 		return $output;
