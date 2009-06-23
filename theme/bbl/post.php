a4f04b57d989d57928319c7036debbadb80102aa
Eric Oestrich <eric@oestrich.org>
2009-06-23 06:25:59 +0000

    Making lines about 80 characters long.
    Commented out the refresh and delete buttons until they fit into a module
    
    git-svn-id: file:///home/oestrich/tmp/img/trunk@1214 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- theme/bbl/post.php
+++ theme/bbl/post.php
@@ -22,12 +22,18 @@ else{
 	$link = URL . "image/" . $image['id'];
 }
 
-$date = $_SESSION['thumbnail'] == true ? date('m/d/Y', $image['postTime'] + 3 * 60 * 60) : date('m/d/Y - h:i:s A', $image['postTime'] + 3 * 60 * 60);
+$date = $_SESSION['thumbnail'] == true ? 
+		date('m/d/Y', $image['postTime'] + 3 * 60 * 60) : 
+		date('m/d/Y - h:i:s A', $image['postTime'] + 3 * 60 * 60);
 
 ?>
 
 	<div class="<?= $lncln->type;?> content">
-		<span class='image_id'><a href="<?echo $link;?>" id="l<?echo $image['id'];?>" name="<?echo $image['id'];?>"><?echo $image['id'];?></a></span>
+		<span class='image_id'>
+			<a href="<?echo $link;?>" id="l<?echo $image['id'];?>" name="<?echo $image['id'];?>">
+				<?echo $image['id'];?>
+			</a>
+		</span>
 		<br />
 		<?=$date;?> 
 		<?foreach($lncln->modules as $module){
@@ -70,13 +76,23 @@ foreach($lncln->modules as $module){
 		echo $module->icon($image['id']) . "\n";
 	}
 }
-
+/**
+ * These do not work in the new system
+ * @todo Move them into some module
+ */
+/*
 if($lncln->user->permissions['refresh'] == 1):?>
-		<a href="<?=$action;?>&amp;refresh=<?echo $image['id'];?>" onclick="return confirm('Are you sure you want to refresh?');"><img src="<?echo URL;?>theme/<?echo THEME;?>/images/refresh.png" alt="Refresh" title="Refresh" style='border: none;'/></a>
+		<a href="<?=$action;?>&amp;refresh=<?echo $image['id'];?>" onclick="return confirm('Are you sure you want to refresh?');">
+			<img src="<?echo URL;?>theme/<?echo THEME;?>/images/refresh.png" alt="Refresh" title="Refresh" style='border: none;'/>
+		</a>
 <?endif;
 if($lncln->user->permissions['delete']):?>
-		<a href="<?=$action;?>&amp;delete=<?echo $image['id'];?>" onclick="return confirm('Are you sure you want to delete this?');"><img src="<?echo URL;?>theme/<?echo THEME;?>/images/delete.png" alt="Delete" title="Delete" style='border: none;'/></a>
+		<a href="<?=$action;?>&amp;delete=<?echo $image['id'];?>" onclick="return confirm('Are you sure you want to delete this?');">
+			<img src="<?echo URL;?>theme/<?echo THEME;?>/images/delete.png" alt="Delete" title="Delete" style='border: none;'/>
+		</a>
 <?endif;
+*/
+
 if($small):?>
 		</div>
 <?endif;
