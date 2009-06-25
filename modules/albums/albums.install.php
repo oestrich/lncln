6e8c83715acf163a21f88f5b008c4d7ce6efd721
Eric Oestrich <eric@oestrich.org>
2009-06-25 18:02:03 +0000

    Albums install file.  Base table required.
    git-svn-id: file:///home/oestrich/tmp/img/trunk@1225 ea04b438-0a61-0410-93d0-9bc465a3bab0

---
+++ modules/albums/albums.install.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * albums.install.php
+ * 
+ * Install file
+ * 
+ * @copyright (C) 2009 Eric Oestrich
+ * @version 0.13.0 $Id$
+ * @license license.txt GNU General Public License version 3
+ * 
+ * @package lncln
+ */
+
+/**
+ * Called when module is first enabled.
+ * @since 0.13.0
+ * 
+ * @return array An array of queries that need to be run
+ */
+function albums_install(){
+	$schema[] = array(
+		'type' => 'CREATE TABLE',
+		'table' => 'albums',
+		'fields' => array(
+			'id' => array(
+				'type' => 'int',
+				'size' => 8,
+				'null' => false,
+				'attributes' => array(
+					'auto_increment' => true,
+					),
+				),
+			'name' => array(
+				'type' => 'varchar',
+				'size' => 50,
+				'null' => false,
+				),
+			),
+		'primary key' => array('id'),
+		);
+	
+	return $schema;
+}
