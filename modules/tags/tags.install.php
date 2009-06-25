5430cf6c247126b3914c80c7e8287d492e4dd67a
Eric Oestrich <eric@oestrich.org>
2009-06-25 18:02:32 +0000

    Tags install file.  Base table required.
    git-svn-id: file:///home/oestrich/tmp/img/trunk@1227 ea04b438-0a61-0410-93d0-9bc465a3bab0

---
+++ modules/tags/tags.install.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * tags.install.php
+ * 
+ * Install file
+ * 
+ * @copyright (C) 2009 Eric Oestrich
+ * @version 0.13.0 $Id$
+ * @license license.txt GNU General Public License version 3
+ * 
+ * @package lncln
+ */
+
+/**
+ * Called when module is first enabled.
+ * @since 0.13.0
+ * 
+ * @return array An array of queries that need to be run
+ */
+function tags_install(){
+	$schema[] = array(
+		'type' => 'CREATE TABLE',
+		'table' => 'tags',
+		'fields' => array(
+			'id' => array(
+				'type' => 'int',
+				'size' => 12,
+				'null' => false,
+				'attributes' => array(
+					'auto_increment' => true,
+					),
+				),
+			'picId' => array(
+				'type' => 'int',
+				'size' => 6,
+				'null' => false,
+				),
+			'tag' => array(
+				'type' => 'text',
+				'null' => false,
+				),
+			),
+		'primary key' => array('id'),
+		);
+	
+	return $schema;
+}
