3097f1929274b296d2f69b51e1734c92f4702e12
Eric Oestrich <eric@oestrich.org>
2009-06-26 03:13:01 +0000

    New settings SQL structure.
    git-svn-id: file:///home/oestrich/tmp/img/trunk@1242 ea04b438-0a61-0410-93d0-9bc465a3bab0

--- mysql.sql
+++ mysql.sql
@@ -119,7 +119,7 @@ CREATE TABLE IF NOT EXISTS `ratings` (
 CREATE TABLE IF NOT EXISTS `settings` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
-  `value` varchar(255) NOT NULL,
+  `value` text NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
